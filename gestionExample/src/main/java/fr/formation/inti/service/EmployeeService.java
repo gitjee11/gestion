6f63c59992b254362767415cb3194e9b95d96ea2
javaBlues <javaformateur@gmail.com>
2022-02-07 11:41:05 +0100

    update service

--- gestionExample/src/main/java/fr/formation/inti/service/EmployeeService.java
+++ gestionExample/src/main/java/fr/formation/inti/service/EmployeeService.java
@@ -2,53 +2,52 @@ package fr.formation.inti.service;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.formation.inti.dao.EmployeeDao;
 import fr.formation.inti.dao.IEmployeeDao;
 import fr.formation.inti.entity.Employee;
 
 @Service
 @Transactional
 public class EmployeeService implements IEmployeeService{
+	private final Log log = LogFactory.getLog(EmployeeService.class);
 	
 	@Autowired
 	private IEmployeeDao dao;
 	
 	public EmployeeService() {
+		log.info("new EmployeeService()");
 	}
 	
 
 	@Override
-	public Integer save(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
+	public Integer save(Employee employee) {		
+		return dao.save(employee);
 	}
 
 	@Override
 	public void update(Employee employee) {
-		// TODO Auto-generated method stub
-		
+		dao.update(employee);
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+		dao.delete(id);
 		
 	}
 
 	@Override
-	public Employee findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Employee findById(Integer id) {		
+		return dao.findById(id);
 	}
 
 	@Override
-	public List<Employee> findAll() {
-		List<Employee> list = dao.findAll();
-		return list;
+	public List<Employee> findAll() {		
+		return dao.findAll();
 	}
 
 }
